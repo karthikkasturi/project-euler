d546caaecdc68c2afbd7ae5a4310fe6857b06e02
Karthik Kasturi <karthik.kasturi@pal.tech>
2023-04-26 18:49:26 +0530

    Largest palindrome product

--- 3.js
+++ 3.js
@@ -1,3 +1,8 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143 ?
+
+
 const leastFactor = (n) => {
     for(let i = 2; i < Math.sqrt(n) + 1; i++) {
         if(n % i == 0) {
---
+++ 4.js
@@ -0,0 +1,42 @@
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+// SOL: A 6 digit palindrome needs to have 11 as a multiple.
+
+
+const isPalindromeNumber = (n) => {
+  const val = n.toString();
+  const rev = val.split("").reverse().join("");
+  return val == rev;
+};
+
+let comp =0;
+
+const min = (a, b) => {
+  return a < b ? a : b;
+};
+
+const findMuls = () => {
+  let maxPal = 0;
+  let vals = [];
+
+  for (let i = 990; i > 100; i -= 11) {
+    const a = min(parseInt(999999 / i), 999);
+    for (let j = a; j > 0; j--) {
+        comp++;
+      if (isPalindromeNumber(i * j)) {
+        if (i * j > maxPal) {
+          maxPal = i * j;
+          vals = [i, j];
+        }
+        break;
+      }
+    }
+  }
+  return vals;
+};
+
+const [m1, m2] = findMuls();
+console.log(m1 * m2);
+console.log(comp)
